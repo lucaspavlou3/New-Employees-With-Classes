1f44266746aa1f7924591a1e9ea92b41ef189d16
lucaspavlou3 <137445683+lucaspavlou3@users.noreply.github.com>
2023-10-05 15:30:47 +0100

    Update new_employees_with_classes.py

--- new_employees_with_classes.py
+++ new_employees_with_classes.py
@@ -1,12 +1,12 @@
 class Employee:
 
-    num_of_emps = 0                  # this is a CLASS variable, and defines the variable for the whole class
+    num_of_emps = 0                                     # this is a CLASS variable, and defines the variable for the whole class
     min_wage = 10.5
     pay = 0
     raise_percentage = 1.05
 
     def __init__(self, first, last, position):          # this is a SELF method, and must take "self" as the first argument
-        self.first = first           # this is an INSTANCE variable, as it changes for each instance of the class 
+        self.first = first                              # this is an INSTANCE variable, as it changes for each instance of the class 
         self.last = last
         self.position = position
         self.email = first.lower() + '.' + last.lower() + '@company.co.uk'
@@ -26,11 +26,11 @@ class Employee:
             return self.pay
 
     @classmethod
-    def from_string(cls, emp_str):              # this is a CLASS method, and must take "cls" as the first argument
+    def from_string(cls, emp_str):                        # this is a CLASS method, and must take "cls" as the first argument
         first, last, position = emp_str.replace("/", "-").split("-")   # if input string uses "/", it is replaced by "-", then split at "-"
         return cls(first, last, position)
 
-    @staticmethod                               # this is a STATIC method, and does not take a "self" or "cls" argument
+    @staticmethod                                         # this is a STATIC method, and does not take a "self" or "cls" argument
     def is_workday(day):
         if day.weekday() == 5 or day.weekday() == 6:
             return False
